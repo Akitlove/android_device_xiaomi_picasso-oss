7924543a0df4de152337bc78b6a84235d9ecd776
Akitlove <416613578@qq.com>
2025-10-16 15:03:55 +0800

    picasso: Restore RIL modifications
    remove
    android.hardware.radio@1.6.vendor
    libprotobuf-cpp-full
    libsqlite.vendor

--- device-common.mk
+++ device-common.mk
@@ -443,13 +443,10 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    libprotobuf-cpp-full \
     librmnetctl \
-    libsqlite.vendor \
-    libxml2
+    libxml2 \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
