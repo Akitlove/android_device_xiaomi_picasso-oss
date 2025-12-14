467acda5e41a633d09e289c67c3f955852b467b0
Akitlove <416613578@qq.com>
2025-12-14 19:00:31 +0800

    picasso: remove COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/picasso/lineage_picasso.mk \
-    $(LOCAL_DIR)/picasso_48m/lineage_picasso_48m.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_picasso-bp2a-user \
-    lineage_picasso_48m-bp2a-user \
-    lineage_picasso-bp2a-userdebug \
-    lineage_picasso_48m-bp2a-userdebug \
-    lineage_picasso-bp2a-eng \
-    lineage_picasso_48m-bp2a-eng
+    $(LOCAL_DIR)/picasso/lineage_picasso.mk
